848972fe33a2e0ee4b8d72ccabe137d21e6c2a2b
Kamasu Paul <kamasupaul@gmail.com>
2021-12-06 11:25:50 +0300

    -- update app to use public  key directly in the app file

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Echo from 'laravel-echo';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = true
 
+
+window.Pusher = require('pusher-js');
+
+window.Echo = new Echo({
+  broadcaster: 'pusher',
+  key: "Kp8mUw.DDCTHQ",//app public key: format='APP_KEY.APP_ID'
+  wsHost: 'realtime-pusher.ably.io',
+  wsPort: 443,
+  disableStats: true,
+  encrypted: true,
+});
 new Vue({
   render: h => h(App),
 }).$mount('#app')
